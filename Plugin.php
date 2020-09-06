3ca5b1ee0c368ad2f286d5c31c15291ff756fb41
Michael <miheider@bluewin.ch>
2020-09-06 19:37:56 +0200

    plugin bauboo.youtube permissions improved

--- Plugin.php
+++ Plugin.php
@@ -6,40 +6,43 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public function registerComponents()
-	{
-		return [
-			'Bauboo\Youtube\Components\Video' => 'video',
-		];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function registerComponents()
+    {
+        return [
+            'Bauboo\Youtube\Components\Video' => 'video',
+        ];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function registerSettings()
-	{
-		return [
-			'general' => [
-				'label'       => 'bauboo.youtube::lang.plugin.name',
-				'description' => 'bauboo.youtube::lang.settings_item.description',
-				'icon'        => 'oc-icon-youtube-play',
-				'class'       => 'Bauboo\Youtube\Models\Settings',
-				'permissions' => ['bauboo.youtube.settings'],
-				'keywords'    => 'bauboo.youtube::lang.settings_item.keywords',
-			],
-		];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function registerSettings()
+    {
+        return [
+            'general' => [
+                'label' => 'bauboo.youtube::lang.plugin.name',
+                'description' => 'bauboo.youtube::lang.settings_item.description',
+                'icon' => 'oc-icon-youtube-play',
+                'class' => 'Bauboo\Youtube\Models\Settings',
+                'permissions' => ['bauboo.youtube.access_settings'],
+                'keywords' => 'bauboo.youtube::lang.settings_item.keywords',
+            ],
+        ];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function registerPermissions()
-	{
-		return [
-			'bauboo.youtube.settings' => ['label' => 'bauboo.youtube::lang.permissions.label'],
-		];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function registerPermissions()
+    {
+        return [
+            'bauboo.youtube.access_settings' => [
+                'tab' => 'bauboo.youtube::lang.plugin.name',
+                'label' => 'bauboo.youtube::lang.permissions.label',
+            ],
+        ];
+    }
 }
